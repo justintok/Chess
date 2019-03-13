872c8d7b9ddf4f616d65731540c7e7e20dd86500
Justin <tokarczj@mail.gvsu.edu>
2019-03-13 16:29:24 -0400

    Added all starter code

---
+++ src/com/company/project3/GUIcodes.java
@@ -0,0 +1,6 @@
+package com.company.project3;
+
+public enum GUIcodes {
+	NoMessage, NotYourTurn, inCheck, Checkmate
+
+}
---
+++ src/com/company/project3/Player.java
@@ -0,0 +1,19 @@
+package com.company.project3;
+
+public enum Player {
+	BLACK, WHITE;
+
+	/**
+	 * Return the {@code Player} whose turn is next.
+	 *
+	 * @return the {@code Player} whose turn is next
+	 */
+	public Player next() {
+		if (this == BLACK)
+			return WHITE;
+		else
+		 	return BLACK;
+		
+	//	return this == BLACK ? WHITE : BLACK;
+	}
+}
