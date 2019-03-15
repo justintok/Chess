98a5b96fbef75e393b848bc1ba9ba16015cbc6cc
Justin <tokarczj@mail.gvsu.edu>
2019-03-15 13:46:50 -0400

    Added nojump to bishop

--- src/com/company/project3/Bishop.java
+++ src/com/company/project3/Bishop.java
@@ -15,11 +15,56 @@ public class Bishop extends ChessPiece {
 
 		if(super.isValidMove(move,board)) {
 			if (Math.abs(move.toRow - move.fromRow) == Math.abs(move.toColumn - move.fromColumn)) {
-				valid = true;
+				if (!noJump(move,board)) {
+					valid = true;
+				}
 			}
 		}
 		return valid;
         // More code is needed
 		
+	}
+	public boolean noJump(Move move,IChessPiece[][] board) {
+		//This method is not working correctly yet. I do not think this is the best way to do this either.
+		//**Update** All directions should be working. I simply changed the < sign to a > in the right/down loop.
+
+		// It is suppose to count the number of spaced you move, then go through all those spaced and find out if there is a
+		//piece there. If there is one, return true.
+
+		int moveRow = (move.fromRow - move.toRow);
+		int moveColumn = (move.fromColumn - move.toColumn);//counts the number of spaces moved
+		if (moveRow > 0) { //going up
+			if (moveColumn < 0) { //gong right
+				for (int i = 1; i < moveRow; i++) {
+					if (board[move.fromRow - i][move.fromColumn + i] != null) {
+						return true;
+					}
+				}
+			}
+			if (moveColumn > 0) { //gong left
+					for (int i = 1; i < moveRow; i++) {
+						if (board[move.fromRow - i][move.fromColumn - i] != null) {
+							return true;
+						}
+					}
+			}
+		}
+			if (moveRow < 0) { //going down
+				if (moveColumn < 0) { //gong right
+					for (int i = -1; i > moveRow; i--) {
+						if (board[move.fromRow - i][move.fromColumn - i] != null) {
+							return true;
+						}
+					}
+				}
+				if (moveColumn > 0) { //gong left
+					for (int i = -1; i > moveRow; i--) {
+						if (board[move.fromRow - i][move.fromColumn + i] != null) {
+							return true;
+						}
+					}
+				}
+			}
+		return false;
 	}
 }
